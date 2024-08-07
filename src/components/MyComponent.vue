b531346a136e37486099d26998708945b1519dfc
Enrique Castellanos <romicze@gmail.com>
2024-08-06 22:29:52 -0600

    First commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'MyComponent',
+    data(){
+        return {
+            message: '¡Hello world!'
+        }
+    }
+}
+</script>
